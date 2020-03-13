7d016bf6edd767f5ca4a3f4bd6b6e3463f5c1d5f
devleni <50588358+devleni@users.noreply.github.com>
2020-03-13 16:56:35 +0100

    Updates

---
+++ precache-manifest.61208c57ccbcf187825e957793343bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aef84e758f5288308fa9fb63e0532fb",
+    "url": "/corporate-buzzwords-app/index.html"
+  },
+  {
+    "revision": "f41aceaa76f1c42b3d95",
+    "url": "/corporate-buzzwords-app/static/css/main.f2c9a7e6.chunk.css"
+  },
+  {
+    "revision": "8af03616691367ff66dd",
+    "url": "/corporate-buzzwords-app/static/js/2.32281202.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/corporate-buzzwords-app/static/js/2.32281202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41aceaa76f1c42b3d95",
+    "url": "/corporate-buzzwords-app/static/js/main.fb834b5e.chunk.js"
+  },
+  {
+    "revision": "b447e653256335da0422",
+    "url": "/corporate-buzzwords-app/static/js/runtime-main.1e12350a.js"
+  }
+]);
